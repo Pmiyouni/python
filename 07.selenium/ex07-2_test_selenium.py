75e027e8e209400bb9ee72e9734b6e677759c7bd
Pmiyouni <joeun125@naver.com>
2023-08-17 23:35:36 +0900

    python 과제응용

--- 07.selenium/ex07-2_test_selenium.py
+++ 07.selenium/ex07-2_test_selenium.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-import urllib.request
 import time
 
 
@@ -15,10 +14,9 @@ search_element.send_keys(Keys.RETURN)
 # btn = driver.find_element(By.XPATH, '//*[@id="search-btn"]')
 # btn.click()
 time.sleep(2)
-# 로딩까지 시간?
 
-# 웹툰 제목 접근
 
+# 웹툰 제목 ,소제목 접근
 web_class=driver.find_elements(By.CSS_SELECTOR, '[class="name line_2"]')  
 web_class2=driver.find_elements(By.CSS_SELECTOR, '[class="sub_text"]') 
 
